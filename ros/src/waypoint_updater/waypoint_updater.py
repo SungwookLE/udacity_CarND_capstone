9b902086c9bf9aeadc358624ad0baa3b815437b3
SungwookLE <joker1251@naver.com>
2021-02-13 23:20:23 +0900

    feat: figure it out what this code means code change a litte ( i think it makes build error )

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -57,7 +57,7 @@ class WaypointUpdater(object):
                 self.puplish_waypoint(closest_waypoint_idx)
             rate.sleep()
 
-    def get_closest_waypoint_id(self):
+    def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
@@ -92,7 +92,7 @@ class WaypointUpdater(object):
         self.base_waypoints = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [
-                [waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints
+                [waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints
             ]
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
